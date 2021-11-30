10f7ef323160508fc9065e43ffc95227ea62ae48
iryamr <iryamuhammadr@gmail.com>
2021-12-01 01:51:19 +0700

    Update Constants.kt

--- app/src/main/java/com/dicoding/kasmee/util/Constants.kt
+++ app/src/main/java/com/dicoding/kasmee/util/Constants.kt
@@ -2,5 +2,6 @@ package com.dicoding.kasmee.util
 
 object Constants {
 
-    const val BASE_URL = "http://192.168.0.104/api/"
+    const val BASE_URL = "http://192.168.0.104:8000/api/"
+    const val USER_TOKEN = "user_token"
 }

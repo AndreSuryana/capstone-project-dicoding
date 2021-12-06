59ccd77604afdb9dae6c881bc93dee4972b3e2e8
Andre Suryana <Andresuryana17@gmail.com>
2021-12-07 02:33:33 +0800

    Adding loading function

--- app/src/main/java/com/dicoding/kasmee/util/Resource.kt
+++ app/src/main/java/com/dicoding/kasmee/util/Resource.kt
@@ -10,5 +10,9 @@ data class Resource<out T>(val status: Status, val data: T?, val message: String
         fun <T> error(message: String): Resource<T> {
             return Resource(Status.ERROR, null, message)
         }
+
+        fun <T> loading(): Resource<T> {
+            return Resource(Status.LOADING, null, null)
+        }
     }
 }

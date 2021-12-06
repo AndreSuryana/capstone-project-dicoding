387e4500117cb4ba71230e3a2cc874f29cb26ab6
Andre Suryana <Andresuryana17@gmail.com>
2021-12-07 03:49:54 +0800

    Initialize view model and getting data from repository

--- app/src/main/java/com/dicoding/kasmee/ui/main/cash/CashViewModel.kt
+++ app/src/main/java/com/dicoding/kasmee/ui/main/cash/CashViewModel.kt
@@ -3,9 +3,33 @@ package com.dicoding.kasmee.ui.main.cash
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dicoding.kasmee.data.model.response.cash.CashResponse
+import com.dicoding.kasmee.data.repository.KasmeeRepository
+import com.dicoding.kasmee.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class CashViewModel : ViewModel() {
+@HiltViewModel
+class CashViewModel @Inject constructor(
+    private val repository: KasmeeRepository
+): ViewModel() {
 
-    private var _text = MutableLiveData("Cash")
-    val text: LiveData<String> = _text
+    private var _cash = MutableLiveData<Resource<CashResponse>>()
+    val cash: LiveData<Resource<CashResponse>> = _cash
+
+    fun generateCash() {
+        viewModelScope.launch {
+            _cash.value = Resource.loading()
+
+            val result = repository.getAllCash()
+
+            if (result.data?.listCash?.isEmpty() == true) {
+                _cash.value = result.message?.let { Resource.error(it) }
+            } else {
+                _cash.value = Resource.success(result.data)
+            }
+        }
+    }
 }

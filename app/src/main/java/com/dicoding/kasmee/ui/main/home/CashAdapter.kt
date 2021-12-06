6f57cbc2d7724644942cc9afdd0680b39d7c5b5c
Andre Suryana <Andresuryana17@gmail.com>
2021-12-07 02:31:02 +0800

    Fixing adapter and adding new component from cash response

--- app/src/main/java/com/dicoding/kasmee/ui/main/home/CashAdapter.kt
+++ app/src/main/java/com/dicoding/kasmee/ui/main/home/CashAdapter.kt
@@ -5,11 +5,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.dicoding.kasmee.R
 import com.dicoding.kasmee.data.model.response.cash.Cash
 import com.dicoding.kasmee.databinding.ItemCashBinding
+import com.dicoding.kasmee.util.StringHelper
 
 class CashAdapter(
-    private val onCLick: (Cash) -> Unit
+    private val onClick: (Cash) -> Unit
 ) : ListAdapter<Cash, CashAdapter.CashViewHolder>(DIFF_CALLBACK) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CashViewHolder {
@@ -24,20 +26,26 @@ class CashAdapter(
 
     override fun getItemCount(): Int = currentList.size
 
-    inner class CashViewHolder(private val binding: ItemCashBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class CashViewHolder(private val binding: ItemCashBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(cash: Cash) {
             binding.apply {
                 /* TODO : Add and then change tvCashTitle value to cash.title,
                  * currently there is no "title" in the cash model
                  */
-                tvCashTitle.text = cash.id.toString()
-                tvIncome.text = cash.transaksi.pemasukan.toString()
-                tvOutcome.text = cash.transaksi.pengeluaran.toString()
-                tvProfit.text = cash.transaksi.keuntungan.toString()
-                tvTarget.text = cash.target.toString()
+                tvCashTitle.text = cash.name
+                tvIncome.text =
+                    itemView.resources.getString(R.string.rupiah_value, StringHelper.formatIntoIDR(cash.totalIncome))
+                tvOutcome.text =
+                    itemView.resources.getString(R.string.rupiah_value, StringHelper.formatIntoIDR(cash.totalOutcome))
+                tvProfit.text =
+                    itemView.resources.getString(R.string.rupiah_value, StringHelper.formatIntoIDR(cash.totalProfit))
+                tvTarget.text =
+                    itemView.resources.getString(R.string.target_percentage, StringHelper.getTargetPercentage(cash.target, cash.totalProfit))
+
                 itemView.setOnClickListener {
-                    onCLick(cash)
+                    onClick(cash)
                 }
             }
         }

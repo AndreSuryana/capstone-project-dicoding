4c017d07107c39de9bdfef22f19248ea173c1bba
Andre Suryana <Andresuryana17@gmail.com>
2021-12-08 02:01:04 +0800

    Adding new const value for index transaction

--- app/src/main/java/com/dicoding/kasmee/util/Constants.kt
+++ app/src/main/java/com/dicoding/kasmee/util/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val BASE_URL = "http://192.168.1.6:8000/api/"
     const val USER_TOKEN = "user_token"
     const val CASH_STARTING_PAGE_INDEX = 1
+    const val TRANSACTION_STARTING_PAGE_INDEX = 1
 }

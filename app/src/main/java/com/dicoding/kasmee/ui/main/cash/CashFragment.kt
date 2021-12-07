351d6e14f2f7461b3589f8a2b4f79263de8fa09e
Andre Suryana <Andresuryana17@gmail.com>
2021-12-07 20:45:04 +0800

    Rename adapter to cashPagingAdapter

--- app/src/main/java/com/dicoding/kasmee/ui/main/cash/CashFragment.kt
+++ app/src/main/java/com/dicoding/kasmee/ui/main/cash/CashFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -13,7 +12,6 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.kasmee.data.model.response.cash.Cash
 import com.dicoding.kasmee.databinding.CashFragmentBinding
-import com.dicoding.kasmee.util.Status
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -24,8 +22,8 @@ class CashFragment : Fragment() {
     private var _binding: CashFragmentBinding? = null
     private val binding get() = _binding
     private val viewModel: CashViewModel by viewModels()
-    private val wideCashAdapter: WideCashAdapter by lazy {
-        WideCashAdapter(::onCashClicked)
+    private val cashPagingAdapter: CashPagingAdapter by lazy {
+        CashPagingAdapter(::onCashClicked)
     }
 
     override fun onCreateView(
@@ -57,26 +55,12 @@ class CashFragment : Fragment() {
         }
         binding?.rvCash?.apply {
             setHasFixedSize(true)
-            adapter = wideCashAdapter
+            adapter = cashPagingAdapter
         }
 
         lifecycleScope.launch {
-            viewModel.generateCash()
-            viewModel.cash.observe(viewLifecycleOwner) { resource ->
-                when (resource.status) {
-                    Status.SUCCESS -> {
-                        hideProgressBar()
-                        resource?.data?.listCash?.let {
-                            wideCashAdapter.submitList(it)
-                        }
-                    }
-                    Status.ERROR -> {
-                        resource?.message?.let { showSnackBar(it) }
-                    }
-                    Status.LOADING -> {
-                        showProgressBar()
-                    }
-                }
+            viewModel.cash.observe(viewLifecycleOwner) {
+                cashPagingAdapter.submitData(viewLifecycleOwner.lifecycle, it)
             }
         }
     }
@@ -86,24 +70,6 @@ class CashFragment : Fragment() {
             Snackbar.make(
                 it,
                 cash.name,
-                Snackbar.LENGTH_SHORT
-            ).show()
-        }
-    }
-
-    private fun showProgressBar() {
-        binding?.progressBar?.isVisible = true
-    }
-
-    private fun hideProgressBar() {
-        binding?.progressBar?.isVisible = false
-    }
-
-    private fun showSnackBar(message: String) {
-        binding?.root?.let {
-            Snackbar.make(
-                it,
-                message,
                 Snackbar.LENGTH_SHORT
             ).show()
         }

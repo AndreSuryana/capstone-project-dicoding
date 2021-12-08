978921e9d71ffa769c6508210546887c29015272
Andre Suryana <Andresuryana17@gmail.com>
2021-12-09 04:11:50 +0800

    Fixing bug force close when rotate screen

--- app/src/main/java/com/dicoding/kasmee/ui/main/transactions/TransactionsViewModel.kt
+++ app/src/main/java/com/dicoding/kasmee/ui/main/transactions/TransactionsViewModel.kt
@@ -1,6 +1,8 @@
 package com.dicoding.kasmee.ui.main.transactions
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.dicoding.kasmee.data.repository.KasmeeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -10,5 +12,5 @@ class TransactionsViewModel @Inject constructor(
     repository: KasmeeRepository
 ): ViewModel() {
 
-    val transaction = repository.getAllTransactionPager()
+    val transaction = repository.getAllTransactionPager().cachedIn(viewModelScope)
 }

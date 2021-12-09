321707379e76eb3d5c6c3e4e84d77a0f0fe89a50
iryamr <iryamuhammadr@gmail.com>
2021-12-09 21:51:31 +0700

    Add load state adapter

---
+++ app/src/main/java/com/dicoding/kasmee/util/KasmeeLoadStateAdapter.kt
@@ -0,0 +1,41 @@
+package com.dicoding.kasmee.util
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.dicoding.kasmee.databinding.LoadStateBinding
+
+class KasmeeLoadStateAdapter(
+    private val retry: () -> Unit
+) : LoadStateAdapter<KasmeeLoadStateAdapter.LoadStateViewHolder>() {
+
+    override fun onBindViewHolder(holder: LoadStateViewHolder, loadState: LoadState) {
+        holder.bind(loadState)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState): LoadStateViewHolder {
+        val binding = LoadStateBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return LoadStateViewHolder(binding)
+    }
+
+    inner class LoadStateViewHolder(private val binding: LoadStateBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.btnRetry.setOnClickListener {
+                retry.invoke()
+            }
+        }
+
+        fun bind(loadState: LoadState) {
+            with(binding) {
+                progressBar.isVisible = loadState is LoadState.Loading
+                btnRetry.isVisible = loadState !is LoadState.Loading
+                tvError.isVisible = loadState !is LoadState.Loading
+            }
+        }
+    }
+}
